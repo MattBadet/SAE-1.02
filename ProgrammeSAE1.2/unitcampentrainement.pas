4ba702d87d0da652ec656eac3236dc806317343a
AimericR <aimericr89@gmail.com>
2021-12-18 15:31:56 +0100

    Camp d'EntrainementV2 (affichage munu1 fini)

--- ProgrammeSAE1.2/unitcampentrainement.pas
+++ ProgrammeSAE1.2/unitcampentrainement.pas
@@ -1,5 +1,5 @@
 unit unitCampEntrainement;
-
+{$codepage utf8}
 {$mode objfpc}{$H+}
 
 interface
@@ -25,20 +25,21 @@ begin
     afficherInterfacePrincipale();
     afficherLieu('Camp d''entrainement de la ville de Brightwood');
 
-    deplacerCurseurXY(30,7);write('Alors que vous approchez de la cantine, l''air s''emplit d''un épais fumet. Viandes, poissons,');
-    deplacerCurseurXY(30,8);write('fruits et légumes dont certains vous sont inconnus sont exposés sur les nombreuses tables');
-    deplacerCurseurXY(30,9);write('qui entourent une cuisine de fortune où des palicos s''affairent à préparer des mets aussi');
-    deplacerCurseurXY(30,10);write('généreux qu''appétissants.');
+    deplacerCurseurXY(30,7);write('Alors que vous vous approchez du camp d''entrainement de la ville, vous commencez à');
+    deplacerCurseurXY(30,8);write('entendre le bruit des épées qui s''entrechoque et des cris de rage des héros. Vous');
+    deplacerCurseurXY(30,9);write('n''êtes apparement pas le seul à chercher à en apprendre plus et a améliorer ses');
+    deplacerCurseurXY(30,10);write('techniques de combat.');
 
-    deplacerCurseurXY(30,12);write('Vous apercevez de nombreux chasseurs assis aux différentes tables de la cantine. Les rires');
-    deplacerCurseurXY(30,13);write('et les chants résonnent créant en ce lieu, une ambiance chaleureuse et rassurante.');
+    deplacerCurseurXY(30,12);write('A votre arrivé, de la poussière vole entre les combattants entre lesquels vous');
+    deplacerCurseurXY(30,13);write('arrivez à vous faufiller pour vous diriger vers le maître d''arme.');
 
-    deplacerCurseurXY(30,15);write('Alors que vous vous asseyez à une table, un palico vous rejoint posant devant vous une cho');
-    deplacerCurseurXY(30,16);write('pe et attendant votre commande.');
+    deplacerCurseurXY(30,15);write('Il vous regarde, debout, une épée a la main dont il se sert comme d''une canne. Il');
+    deplacerCurseurXY(30,16);write('vous demande alors si vous êtes ici pour apprendre ses techniques spécial, contre');
+    deplacerCurseurXY(30,17);write('une certaine somme d''argent bien évidemment.');
 
     couleurTexte(White);
     deplacerCurseurZoneAction(1);write('Que souhaitez-vous faire ?');
-    deplacerCurseurZoneAction(3);write('     1/ Commander un plat');
+    deplacerCurseurZoneAction(3);write('     1/ Apprendre une compétence');
 
     deplacerCurseurZoneAction(6);write('     0/ Retourner sur la place principale');
 
